8e7318f4e1f6bf0424491d25cd7e3b96625b5362
Andreas Reiter <andreas.reiter@student.tugraz.at>
2010-01-25 13:50:57 +0100

    Added connectivity stub classes, to be extended soon :-)

---
+++ tpm_core/Connection/IFrontEndConnection.cs
@@ -0,0 +1,23 @@
+﻿//
+//
+// Author: Andreas Reiter <andreas.reiter@student.tugraz.at>
+// Author: Georg Neubauer <georg.neubauer@student.tugraz.at>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iaik.Tc.Tpm.Connection
+{
+    /// <summary>
+    /// Abstracts the Connection to the TPM - Library
+    /// This is the connection from the clients to the the server or
+    /// an accepted connection on the server side
+    /// </summary>
+    public interface IFrontEndConnection
+    {
+
+
+    }
+}
---
+++ tpm_core/Connection/IFrontEndServerListener.cs
@@ -0,0 +1,22 @@
+﻿//
+//
+// Author: Andreas Reiter <andreas.reiter@student.tugraz.at>
+// Author: Georg Neubauer <georg.neubauer@student.tugraz.at>
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iaik.Tc.Tpm.Connection
+{
+    /// <summary>
+    /// Abstracts the listening connection on the server side.
+    /// there is typically only one listeniong connection of each 
+    /// supported type on the server
+    /// </summary>
+    public interface IFrontEndServerListener
+    {
+    }
+}
