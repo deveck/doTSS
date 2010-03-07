3a2fe8f26284c3baed7e18f21bc9be660affd205
Georg Neubauer <georg.neubauer@student.tugraz.at>
2010-03-07 01:22:24 +0100

    renamed tpm_lib_server to tpm_lib_core, because it could be used also outside the server. further work on parameters and command request.

--- tpm_lib/tpm_lib_common/interface/Parameters.cs
+++ tpm_lib/tpm_lib_common/interface/Parameters.cs
@@ -6,19 +6,47 @@ using System.Collections.Generic;
 
 namespace Iaik.Tc.Tpm.library.common
 {
+	/// <summary>
+	/// This class represents the parameters list, that is passed from the client to the server
+	/// used by TPMCommands
+	/// </summary>
 	public class Parameters
 	{
+		/// <summary>
+		/// Storage of the parameters list
+		/// </summary>
 		private IDictionary<String, Object> encapsulated_ = new Dictionary<String, Object>();
 
+		/// <summary>
+		/// The standard ctor
+		/// </summary>
 		public Parameters ()
 		{
 		}
 		
+		/// <summary>
+		/// Add a value to parameters
+		/// </summary>
+		/// <param name="key">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <param name="val">
+		/// A <see cref="Object"/>
+		/// </param>
 		public void AddValue(String key, Object val)
 		{
 			encapsulated_.Add(key, val);
 		}
 		
+		/// <summary>
+		/// Get the value of a specified parameter.
+		/// </summary>
+		/// <param name="key">
+		/// A <see cref="String"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="T"/>
+		/// </returns>
 		public T GetValueOf<T>(String key)
 		{
 			return (T) encapsulated_[key];
--- tpm_lib/tpm_lib_common/interface/TPMCommandRequest.cs
+++ tpm_lib/tpm_lib_common/interface/TPMCommandRequest.cs
@@ -6,15 +6,41 @@ using System.Collections.Generic;
 
 namespace Iaik.Tc.Tpm.library.common
 {
-
-
+	/// <summary>
+	/// Represents the command request for a specific TPM command, that should
+	/// be processed by the TPM library.
+	/// </summary>
 	public class TPMCommandRequest
 	{
 
 		private String name_;
-		private IDictionary<String, object> parameters_;
-		public TPMCommandRequest ()
+		private Parameters params_;
+		public TPMCommandRequest()
 		{
 		}
+		
+		public TPMCommandRequest(String commandIdentifier, Parameters param)
+		{
+			name_ = commandIdentifier;
+			params_ = param;
+		}
+		
+		public Parameters Parameters
+		{
+			get
+			{
+				return params_;				
+			}
+			
+			set
+			{
+				if(value == null)
+					throw new ArgumentNullException("params_", "TPMCommandRequest params_ must not be null");
+				params_ = value;
+			}	
+			
+		}
+		
+		//public 
 	}
 }
