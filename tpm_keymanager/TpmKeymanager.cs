9beede078e87c4b8d79eca825b818699cf39d4cf
Georg Neubauer <georg.neubauer@student.tugraz.at>
2010-04-14 22:25:48 +0200

    Added more GetCapability features (Keys, Dir[depricated]) Added a test for GetCapability
    Use switch-case in GetCapability file instead of if-else

--- tpm_keymanager/TpmKeymanager.cs
+++ tpm_keymanager/TpmKeymanager.cs
@@ -3,17 +3,41 @@
 
 using System;
 using Mono.Security.X509;
+using Iaik.Tc.TPM.Library;
 
 namespace Iaik.Tc.TPM.Keymanager
 {
 
 
-	public class TpmKeymanager
+	public class TpmKeymanager : IDisposable
 	{
-
-		public TpmKeymanager ()
+		TPMWrapper _tpm = null;
+		
+		bool _isDisposed = false;
+		
+		private TpmKeymanager ()
 		{
 			
 		}
+		
+		public TpmKeymanager (TPMWrapper tpm)
+		{
+			_tpm = tpm;
+		}
+		
+		
+		#region IDisposable implementation
+		void IDisposable.Dispose ()
+		{
+			lock(this)
+				if(_isDisposed)
+					throw new ObjectDisposedException("TPMKeymanager object is disposed");
+			_tpm.Dispose();
+			_isDisposed = true;
+		}
+		
+		#endregion
+
+		
 	}
 }
